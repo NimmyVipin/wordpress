3e6824e1941e821875db8435ee964663777401ee
ruby <NimmyVipin>
2016-09-18 20:42:41 +0530

    New wordpress theme added

---
+++ footer.php
@@ -0,0 +1,39 @@
+	<div class="footer">
+				<div>
+					<ul>
+						<li>
+							<h1>FEATURED VIDEO</h1>
+							<a href="blog.html"><img src="images/mars-rover.jpg" alt=""><span></span></a>
+						</li>
+						<li>
+							<h1>LATEST BLOG</h1>
+							<ul>
+								<li>
+									<a href="blog.html"><img src="images/finding-planet.jpg" alt=""></a>
+									<h1>FINDING PLANET X-123</h1>
+									<span>FEBRUARY 6, 2023</span>
+									<a href="blog.html" class="more">Read More</a>
+								</li>
+								<li>
+									<a href="blog.html"><img src="images/new-satellitedish.jpg" alt=""></a>
+									<h1>NEW SATELLITE DISH</h1>
+									<span>FEBRUARY 3, 2023</span>
+									<a href="blog.html" class="more">Read More</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			
+			<div class="footnote">
+				<div>
+					<p>Copyright <?php echo date('Y'); ?></p>
+			</div>
+		</div>
+	</div>
+    	<?php wp_footer(); ?>
+</body>
+</html>
